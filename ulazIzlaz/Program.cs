8bf37dc1dda32b1fa837bd3f869a7651780cf6ef
kPavlicevic <katarina.pavlicevic5@gmail.com>
2023-06-22 21:42:06 +0200

    ulazizlaz

---
+++ ulazIzlaz/Program.cs
@@ -0,0 +1,23 @@
+﻿//ulaz
+//Console.Write("Unesi broj: ");
+//int broj=int.Parse(Console.ReadLine());
+
+//algoritam
+//broj=broj+7;
+
+//ilaz
+//Console.WriteLine(broj);
+
+//Program unosi dva decimalna broja
+//Program ispisuje njihov zbroj
+
+Console.WriteLine("Unesi prvi broj: ");
+double b1=double.Parse(Console.ReadLine());
+
+Console.WriteLine("Unesi drugi broj: ");
+double b2=double.Parse(Console.ReadLine());
+
+double rez = b1 + b2;
+Console.WriteLine(rez);
+
+
