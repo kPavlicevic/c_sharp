69363f4b01f8026fc634e2417637b9b22432bf5c
kPavlicevic <katarina.pavlicevic5@gmail.com>
2023-06-22 21:32:21 +0200

    vjezba

---
+++ vjezba/varijable/Program.cs
@@ -0,0 +1,14 @@
+﻿// deklarirajte 4 varijable različitih tipova podataka 
+//svakom od njih dodajte vrijednost po želji
+//sve ih odjednom ispišite jedno pored drugog
+
+string znak = "Znak";
+bool vrijednost = true;
+float broj = 1.5f;
+double decimalni = 54.2;
+
+Console.WriteLine("{0},{1},{2},{3}",znak,vrijednost,broj,decimalni);
+
+
+
+while (Console.ReadLine() == "/n") ;
