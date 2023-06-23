a4392683a4c8238987e6d44cff52d2e1293b5b78
kPavlicevic <katarina.pavlicevic5@gmail.com>
2023-06-23 18:03:02 +0200

    if

---
+++ materijali/uvjetnoGrananjeIF/Program.cs
@@ -0,0 +1,36 @@
+﻿int i = 0;
+bool uvjet = i > 0;
+
+if (uvjet) ; 
+{
+    Console.WriteLine("Veći od 0");
+}
+
+//koristiti ćemo ovu sintaksu
+
+if (i > 0) 
+{
+    Console.WriteLine("Opet veće od 0");
+}
+
+//ako se if odnosi na jednu liniju ne trebaju {}
+
+if (!uvjet)
+    Console.WriteLine("Nije veće od 0");
+    Console.WriteLine("Ovo isto ne bi trebalo biti veće od 0");
+
+//koristiti ćemo uvijek {}
+
+//opcionalna sintaksa
+
+string grad = "Osijek";
+
+if (grad == "Osijek")
+{
+    Console.WriteLine("SUPER");
+}
+else 
+{
+    Console.WriteLine("OK");
+}
+
