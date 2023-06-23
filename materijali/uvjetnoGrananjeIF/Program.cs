ed37e871e11596918b5d06f43955fb796d9e2717
kPavlicevic <katarina.pavlicevic5@gmail.com>
2023-06-23 18:12:25 +0200

    paran neparan

--- materijali/uvjetnoGrananjeIF/Program.cs
+++ materijali/uvjetnoGrananjeIF/Program.cs
@@ -34,3 +34,59 @@ else
     Console.WriteLine("OK");
 }
 
+int b = 0;
+
+if (grad != "Zagreb")
+{
+    b = b + 1; //broj b se povećava za 1
+}
+else if (grad == "Split")
+{
+    b += 1; //broj b se uvećava za 1
+}
+else if (grad == "Osijek")
+{
+    b += 2; //broj b se uvećava za 2
+}
+else 
+{
+    b++; //broj b se uvećava za 1
+}
+Console.WriteLine(+b);
+
+//if možemo ugnijezditi
+i = 0; b = 2;
+if (i > 0) 
+{
+    if (b == 2) 
+    {
+        Console.WriteLine("Oba uvjeta su zadovoljena");
+    }
+}
+
+//korištenje logičkih operatora
+
+if (i > 0 && b == 2) 
+{
+    Console.WriteLine("Oba uvjeta su zadovoljena");
+}
+
+if (i == 4 || b < 0) 
+{
+}
+
+int g = 10;
+
+if (g % 2 == 0)
+{
+    Console.WriteLine("Broj je paran");
+}
+else 
+{ 
+    Console.WriteLine("Broj je neparan");
+}
+
+//? operator - inline if
+//ovaj jedan red je istovjetan linijama 80-86
+
+Console.WriteLine("Broj je " + (g % 2 == 0 ? "" : "ne") + "paran");
