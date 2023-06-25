bf2c8b66a39ec4cc1d4453333b75d9e3188b9572
kPavlicevic <katarina.pavlicevic5@gmail.com>
2023-06-25 21:16:41 +0200

    vježba switch

---
+++ vjezba/uvjetnogrananjeSwitch/Program.cs
@@ -0,0 +1,29 @@
+﻿// Zadatak 1
+// Program unosi ime mjesta iz jedne od
+// 4 slavonske županije
+// Za uneseno ime mjesta program
+// ispisuje ime županije
+
+
+Console.WriteLine("Unesi ime grada: ");
+string grad = Console.ReadLine();
+
+switch (grad)
+{
+   case "Osijek":
+        Console.WriteLine("Osječko-baranjska županija");
+        break;
+    case "Pleternica":
+        Console.WriteLine("Požeško-slavonska županija");
+        break;
+    case "Ivankovo":
+        Console.WriteLine("Vukovarsko-srijemska županija");
+        break;
+    case "Slavonski brod":
+        Console.WriteLine("Brodsko-posavska županija");
+        break;
+    default:
+        Console.WriteLine("nije grad");
+        break;
+}
+
